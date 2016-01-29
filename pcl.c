115d8863e88f2d7d2bc5e13f9922fe1fd1a3314c
Gaby Stallo <gaby86@gmail.com>
2016-01-29 17:16:11 -0300

    misc: intercambiar nodos (reapuntando)

--- pcl.c
+++ pcl.c
@@ -62,7 +62,7 @@ struct nodo * unqueue(struct nodo ** cola) {
 
 int agregar(struct nodo ** lista, struct nodo * nodo, unsigned int posicion) {
 
-  int i;
+  unsigned int i;
   struct nodo ** apuntador;
 
   apuntador = lista;
@@ -79,7 +79,7 @@ int agregar(struct nodo ** lista, struct nodo * nodo, unsigned int posicion) {
 
 struct nodo * quitar(struct nodo ** lista, unsigned int posicion) {
 
-  int i;
+  unsigned int i;
   struct nodo ** apuntador;
   struct nodo * aux;
 
@@ -95,6 +95,57 @@ struct nodo * quitar(struct nodo ** lista, unsigned int posicion) {
 
 }
 
+
+//misceláneas de listas
+
+int intercambiar(struct nodo ** lista, unsigned int p1, unsigned int p2) {
+
+  //1 es el primer nodo del intercambio y 2 es el segundo nodo del intercambio
+
+  unsigned int i;
+  struct nodo ** apuntador1;
+  struct nodo ** apuntador2;
+
+  struct nodo * aux, aux2;
+
+  apuntador1=apuntador2=lista;
+
+  if(p1==p2) return 0;
+  if(p2<p1) {
+    i=p2;
+    p2=p1;
+    p1=i;
+  }
+
+  for(i=0;*apuntador1 && (**apuntador1).sig && i<p1;i++)
+    apuntador1=&(**apuntador1).sig;
+  for(i=0;*apuntador2 && (**apuntador2).sig && i<p2;i++)
+    apuntador2=&(**apuntador2).sig;
+
+
+  aux=(**apuntador1).sig;  //en caso que sean adyacentes, ahora aux es un puntero al 2
+
+  (**apuntador1).sig=(**apuntador2).sig; //ojo, esto cambió a apuntador1
+  if(p2>p1+1)
+    (**apuntador2).sig=aux;
+  else
+    //si son adyacentes (aux es un backup del sig del 1, que ahora es lo unico que apunta al 2 (ya que *apuntador2, murió accidentalmente al reemplazar (**apuntador1).sig))
+    (*aux).sig=*apuntador1;
+
+  if(p2>p1+1) {
+    //si no son adyacentes, es facil
+    aux=*apuntador1;
+    *apuntador1=*apuntador2;
+    *apuntador2=aux;
+  } else {
+    //si son adyacentes, solo me falta corregir que el apuntador al 1, ahora apunte al 2
+    *apuntador1=aux; //aux era un backup del puntero al 2
+  }
+
+  return 0;
+
+}
+
 int main() {
 
   struct nodo * pila;
@@ -103,12 +154,14 @@ int main() {
   struct nodo nodo1;
   struct nodo nodo2;
   struct nodo nodo3;
+  struct nodo nodo4;
 
   struct nodo * aux;
 
   nodo1.val=1;
   nodo2.val=2;
   nodo3.val=3;
+  nodo4.val=4;
 
   //pila
   printf("-- pila --\n");
@@ -140,6 +193,17 @@ int main() {
   aux=quitar(&lista,1);
   printf("%i\n",(*aux).val);
   while(aux=quitar(&lista,0))
+    printf("%i\n",(*aux).val);
+
+  //lista misc
+  printf("-- lista misc --\n");
+  init(&lista);
+  agregar(&lista,&nodo1,0);
+  agregar(&lista,&nodo3,1);
+  agregar(&lista,&nodo2,1);
+  agregar(&lista,&nodo4,3);
+  intercambiar(&lista, 1, 3);
+  while(aux=pop(&lista))
     printf("%i\n",(*aux).val);
 
   return 0;
